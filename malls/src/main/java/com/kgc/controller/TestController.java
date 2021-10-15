2c27abd317c3e8501233b3765a0c7088100e387d
hanchaofa <2285460357@qq.com>
2021-10-15 18:35:12 +0800

    12312313

--- malls/src/main/java/com/kgc/controller/TestController.java
+++ malls/src/main/java/com/kgc/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-
+    // wdsdads
     @RequestMapping("/login")
     public String getshiw(Model model){
         //vjhggjg
