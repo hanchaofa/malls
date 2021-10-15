1cd10e3fd34587ba766d2cff644005c5e65954c5
hanchaofa <2285460357@qq.com>
2021-10-15 17:56:01 +0800

    first update by han

--- malls/src/main/java/com/kgc/controller/TestController.java
+++ malls/src/main/java/com/kgc/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.kgc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
+
     @RequestMapping("/login")
-    public String getshiw(){
+    public String getshiw(Model model){
+        //vjhggjg
+        System.out.println("testss");
+        model.addAttribute("","");
         return "page/admin/loginPage";
     }
 }
