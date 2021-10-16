55680e3d56cded390ef5a8ff52f62cf9803b0e02
RenBL <2929433379@qq.com>
2021-10-16 10:52:34 +0800

    11

--- malls/src/main/java/com/kgc/pojo/Admin.java
+++ malls/src/main/java/com/kgc/pojo/Admin.java
@@ -1,7 +1,11 @@
 package com.kgc.pojo;
 
 import lombok.Data;
-
+@Data
 public class Admin {
-
+    private int    adminId;
+    private  String   adminName;
+    private  String   adminNickname;
+    private  String   adminPassword;
+    private  String   adminProfilePictureSrc;
 }
