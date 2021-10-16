2dc832d078a5915e2b507d8fdac71b4b9773972c
hanchaofa <2285460357@qq.com>
2021-10-16 17:31:55 +0800

    10/16-2

--- malls/src/main/java/com/kgc/service/AdminServiceImpl.java
+++ malls/src/main/java/com/kgc/service/AdminServiceImpl.java
@@ -12,11 +12,6 @@ public class AdminServiceImpl  {
 
 
     public Admin getLogin(String adminName, String adminPassword) {
-        Admin admin = new Admin();
-        admin.setAdminName(adminName);
-        admin.setAdminPwd(adminPassword);
-        // 调用dao层方法，根据用户名和密码获取用户对象
-        Admin loginAdmin = adminMapper.getAdmin(admin);
-        return loginAdmin;
+       return null;
     }
 }
